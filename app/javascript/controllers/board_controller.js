715e8e43df0155e1b375404490e899955a319ba9
MrBraz10 <nekita10@mail.ru>
2022-10-27 13:28:10 +0300

    Adding boards and CSS for them

--- app/javascript/controllers/board_controller.js
+++ app/javascript/controllers/board_controller.js
@@ -2,14 +2,64 @@ import {Controller} from "@hotwired/stimulus"
 
 export default class extends Controller {
     connect() {
+        const boards =
+            [
+                {
+                    "id"    : "board-id-1",
+                    "title" : "Board Title 1",
+                    "class" : "bg-red-700, text-white",
+                    "item"  : [
+                        {
+                            "id"      : "item-id-1",
+                            "title"   : "Item 1",
+                            "username": "username1"
+                        },
+                        {
+                            "id"      : "item-id-2",
+                            "title"   : "Item 2",
+                            "username": "username2"
+                        }
+                    ]
+                },
+                {
+                    "id"    : "board-id-2",
+                    "title" : "Board Title 2",
+                    "class" : "bg-blue-700, text-white",
+                    "item"  : [
+                        {
+                            "id"      : "item-id-1",
+                            "title"   : "Item 1",
+                            "username": "username1"
+                        },
+                        {
+                            "id"      : "item-id-2",
+                            "title"   : "Item 2",
+                            "username": "username2"
+                        }
+                    ]
+                },
+                {
+                    "id"    : "board-id-3",
+                    "title" : "Board Title 3",
+                    "class" : "bg-green-700, text-white",
+                    "item"  : [
+                        {
+                            "id"      : "item-id-1",
+                            "title"   : "Item 1",
+                            "username": "username1"
+                        },
+                        {
+                            "id"      : "item-id-2",
+                            "title"   : "Item 2",
+                            "username": "username2"
+                        }
+                    ]
+                }
+                ];
+
         var kanban = new jKanban({
             element: '#board',                                           // selector of the kanban container
-            gutter: '15px',                                       // gutter of the board
-            widthBoard: '250px',                                      // width of the board
-            responsivePercentage: false,                                    // if it is true I use percentage in the width of the boards and it is not necessary gutter and widthBoard
-            dragItems: true,                                         // if false, all items are not draggable
-            boards: [],                                           // json of boards
-            dragBoards: true,                                         // the boards are draggable, if false only item can be dragged
+            boards: boards,                                           // json of boards
             itemAddOptions: {
                 enabled: false,                                              // add a button to board for easy item creation
                 content: '+',                                                // text or html content of the board button
@@ -24,22 +74,14 @@ export default class extends Controller {
                 customHandler: "<span class='item_handle'>+</span> %title% "  // your entirely customized handler. Use %title% to position item title
                                                                               // any key's value included in item collection can be replaced with %key%
             },
-            click: function (el) {
-            },                             // callback when any board's item are clicked
-            context: function (el, event) {
-            },                      // callback when any board's item are right clicked
-            dragEl: function (el, source) {
-            },                     // callback when any board's item are dragged
-            dragendEl: function (el) {
-            },                             // callback when any board's item stop drag
-            dropEl: function (el, target, source, sibling) {
-            },    // callback when any board's item drop in a board
-            dragBoard: function (el, source) {
-            },                     // callback when any board stop drag
-            dragendBoard: function (el) {
-            },                             // callback when any board stop drag
-            buttonClick: function (el, boardId) {
-            },                     // callback when the board's button is clicked
+            click: function (el) {},                             // callback when any board's item are clicked
+            context: function (el, event) {},                      // callback when any board's item are right clicked
+            dragEl: function (el, source) {},                     // callback when any board's item are dragged
+            dragendEl: function (el) {},                             // callback when any board's item stop drag
+            dropEl: function (el, target, source, sibling) {},    // callback when any board's item drop in a board
+            dragBoard: function (el, source) {},                     // callback when any board stop drag
+            dragendBoard: function (el) {},                             // callback when any board stop drag
+            buttonClick: function (el, boardId) {},                     // callback when the board's button is clicked
             propagationHandlers: [],                                         // the specified callback does not cancel the browser event. possible values: "click", "context"
         });
         console.log('kanban', kanban);
