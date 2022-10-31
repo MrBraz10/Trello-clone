72bd0d2adc894eeab7eeab8fb2dfc5c2e9db5f92
MrBraz10 <nekita10@mail.ru>
2022-10-31 18:23:57 +0300

    Adding new list form

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
 
   get "dashboard", to: "dashboard#index"
 
-  resources :boards, only: [:new, :edit, :create, :update, :destroy, :show]
+  resources :boards do
+    resources :lists
+  end
 
   namespace :api do
     resources :boards do
