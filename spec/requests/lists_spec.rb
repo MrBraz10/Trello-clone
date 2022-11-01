590456df71597e2a7e8ba517106189e137f8f215
MrBraz10 <nekita10@mail.ru>
2022-11-01 17:05:53 +0300

    Deleting & Editing a List

--- spec/requests/lists_spec.rb
+++ spec/requests/lists_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe "Boards", type: :request do
   let(:user) { create(:user) }
   let(:board) { create(:board, user: user) }
-  let(:list) { create(:list, board: board)}
+  let(:list) { create(:list, board: board) }
 
   before do
     sign_in user
@@ -16,12 +16,12 @@ RSpec.describe "Boards", type: :request do
     end
   end
 
-    describe "GET edit" do
-      it "succeeds" do
-        get edit_board_list_path(board, list)
-        expect(response).to have_http_status(:success)
-      end
+  describe "GET edit" do
+    it "succeeds" do
+      get edit_board_list_path(board, list)
+      expect(response).to have_http_status(:success)
     end
+  end
 
   describe "POST create" do
     context "with valid params" do
@@ -51,45 +51,41 @@ RSpec.describe "Boards", type: :request do
     end
   end
 
-  pending do
-    describe "PUT update" do
-      context "with valid params" do
-        it "updates the board and redirects" do
-          expect do
-            put board_path(board), params: {
-              board: {
-                name: "Updated Board"
-              }
+  describe "PUT update" do
+    context "with valid params" do
+      it "updates the board and redirects" do
+        expect do
+          put board_list_path(board, list), params: {
+            list: {
+              title: "Updated List"
             }
-          end.to change { board.reload.name }.to("Updated Board")
-          expect(response).to have_http_status(:redirect)
-        end
+          }
+        end.to change { list.reload.title }.to("Updated List")
+        expect(response).to have_http_status(:redirect)
       end
+    end
 
-      context "with invalid params" do
-        it "does not updates the board and redirects" do
-          expect do
-            put board_path(board), params: {
-              board: {
-                name: ""
-              }
+    context "with invalid params" do
+      it "does not updates the board and redirects" do
+        expect do
+          put board_list_path(board, list), params: {
+            list: {
+              title: ""
             }
-          end.not_to change { board.reload.name }
-          expect(response).to have_http_status(:success)
-        end
+          }
+        end.not_to change { list.reload.title }
+        expect(response).to have_http_status(:success)
       end
     end
   end
 
-  pending do
-    describe "DELETE destroy" do
-      it "deletes the board record" do
-        board
-        expect do
-          delete board_path(board)
-        end.to change { Board.count }.by(-1)
-        expect(response).to have_http_status(:redirect)
-      end
+  describe "DELETE destroy" do
+    it "deletes the board record" do
+      list
+      expect do
+        delete board_list_path(board, list), headers: { 'ACCEPT': 'application/json' }
+      end.to change { List.count }.by(-1)
+      expect(response).to have_http_status(:success)
     end
   end
 end
